199abf733b9df494b761cbed392b3ca52722ed01
Rana Jahanzaib <work@withrana.com>
2023-06-07 18:24:52 +0500

    Anas/feat/assets from alloc (#31)
    * working on getting assets from allocation
    
    * listing shared files working
    
    * Update src/assets.js
    
    * done loading assets from allocation
    
    * refactor
    
    * refactor
    
    * refactor
    
    * added rearrangement of array so we can download assets that show first first
    
    ---------
    
    Co-authored-by: manasnisar <anasnisar980@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    main: "./src/index.js",
+    assets: "./src/assets.js"
+  },
   output: {
-    filename: "main.js",
+    filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
   },
   mode: "development",
