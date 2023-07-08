ea94c41191e88d5bb3fedcbd18db3a27ee9afa28
ranajahanzaib <iam@ranajahanzaib.com>
2023-07-08 05:54:08 +0500

    Disable Logs for Production Mode
    Disable Logs
    
    Minor Fix
    
    Fix Build

--- src/player.js
+++ src/player.js
@@ -43,9 +43,9 @@ export const startPlay = async ({
 
   // const mimeCodecs = `${mimeType};
   // codecs="${muxjs.mp4.probe.tracks(buf).map(t => t.codec).join(",")}"`;
-  console.log(
-    `playlist: isFragmented:${isFragmented} mimeCodecs:${mimeCodecs}`
-  );
+  // console.log(
+  //   `playlist: isFragmented:${isFragmented} mimeCodecs:${mimeCodecs}`
+  // );
 
   if (isFragmented && MediaSource.isTypeSupported(mimeCodecs)) {
     return playChunks({ videoElement, buf, mimeCodecs });
